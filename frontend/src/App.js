5c8f8509f20ed1cc2c9fc8d8598f3a366a404b32
david hutchinson <tubcase@hotmail.com>
2020-06-09 21:45:07 +0200

    Init Facebook commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import { observer } from 'mobx-react';
 import './App.css';
-import UserStore from './stores/UserStore';
-import LoginForm from './components/LoginForm';
-import SubmitButton from './components/SubmitButton';
+import Facebook from './components/Facebook';
+// import UserStore from './stores/UserStore';
+// import LoginForm from './components/LoginForm';
+// import SubmitButton from './components/SubmitButton';
 
 // import { 
 //   BrowserRouter as Router, 
@@ -19,91 +20,105 @@ import SubmitButton from './components/SubmitButton';
 // import UsersPage from "./pages/users";
 // import LoginPage from "./pages/login";
 
-class App extends Component{
-  
-  async componentDidMount(){
-    try{
-      let res = await fetch('/isLoggedIn', {
-        method: 'post',
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
-        }
-      });
-
-      let result = await res.json();
-
-      if(result && result.success){
-        UserStore.loading = false;
-        UserStore.isLoggedIn = true;
-        UserStore.username = result.username;
-      }
-      else{
-        UserStore.loading = false;
-        UserStore.isLoggedIn = false;
-      }
-    }
-    catch(e){
-        UserStore.loading = false;
-        UserStore.isLoggedIn = false;
-    }
-  }
-  
-  async doLogout(){
-    try{
-
-      let res = await fetch('/logout', {
-        method: 'post',
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
-        }
-      });
-
-      let result = await res.json();
-
-      if(result && result.success){
-        UserStore.isLoggedIn = false;
-        UserStore.username = '';
-      }
-    }
-    catch(e){
-        console.log(e);
-    }
-  }
-
-  render() {
-    if(UserStore.loading){
-      return (
-        <div className="app">
-          <div className='container'>
-            Loading, please wait...
-          </div>
+class App extends Component {
+  render(){
+    return (
+      <div className="app">
+        <div className="container">
+          <p>
+            Facebook Authentication
+          </p>
+          <Facebook/>
         </div>
-      );
-    }
-    else{
-      if(UserStore.isLoggedIn){
-        return (
-          <div className="app">
-            <div className='container'>
-              Welcome {UserStore.username}
-              <SubmitButton 
-                text={'Log out'}
-                disabled={false}
-                onClick={ () => this.doLogout() }
-              />
-            </div>
-          </div>
-        );
-      }
-      return (
-        <div className="app">
-          <div className='container'>
-            <LoginForm />
-          </div>
-        </div>
-      );
+      </div>
+    );
+  }
+}
+
+// class App extends Component{
+  // async componentDidMount(){
+  //   try{
+  //     let res = await fetch('/isLoggedIn', {
+  //       method: 'post',
+  //       headers: {
+  //         'Accept': 'application/json',
+  //         'Content-Type': 'application/json'
+  //       }
+  //     });
+
+  //     let result = await res.json();
+
+  //     if(result && result.success){
+  //       UserStore.loading = false;
+  //       UserStore.isLoggedIn = true;
+  //       UserStore.username = result.username;
+  //     }
+  //     else{
+  //       UserStore.loading = false;
+  //       UserStore.isLoggedIn = false;
+  //     }
+  //   }
+  //   catch(e){
+  //       UserStore.loading = false;
+  //       UserStore.isLoggedIn = false;
+  //   }
+  // }
+  
+  // async doLogout(){
+  //   try{
+
+  //     let res = await fetch('/logout', {
+  //       method: 'post',
+  //       headers: {
+  //         'Accept': 'application/json',
+  //         'Content-Type': 'application/json'
+  //       }
+  //     });
+
+  //     let result = await res.json();
+
+  //     if(result && result.success){
+  //       UserStore.isLoggedIn = false;
+  //       UserStore.username = '';
+  //     }
+  //   }
+  //   catch(e){
+  //       console.log(e);
+  //   }
+  // }
+
+  // render() {
+  //   if(UserStore.loading){
+  //     return (
+  //       <div className="app">
+  //         <div className='container'>
+  //           Loading, please wait...
+  //         </div>
+  //       </div>
+  //     );
+  //   }
+  //   else{
+  //     if(UserStore.isLoggedIn){
+  //       return (
+  //         <div className="app">
+  //           <div className='container'>
+  //             Welcome {UserStore.username}
+  //             <SubmitButton 
+  //               text={'Log out'}
+  //               disabled={false}
+  //               onClick={ () => this.doLogout() }
+  //             />
+  //           </div>
+  //         </div>
+  //       );
+  //     }
+  //     return (
+  //       <div className="app">
+  //         <div className='container'>
+  //           <LoginForm />
+  //         </div>
+  //       </div>
+  //     );
     //   return <Router>
     //   <Switch> 
     //     <Route exact path="/" component={MainPage}/>
@@ -113,8 +128,8 @@ class App extends Component{
     //     <Redirect to="/404"/>
     //   </Switch>
     // </Router>;
-    }
-  }
-}
+  //   }
+  // }
+// }
 
 export default observer (App);
